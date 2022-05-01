72f4bae6861a420986b5d344873b37a86268e3fd
rwiebe99 <richard.wiebe@student.fhws.de>
2022-05-01 16:02:21 +0200

    master funktioniert

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -7,38 +7,9 @@ sap.ui.define( ["sap/ui/core/mvc/Controller", "sap/ui/Device"], function (Contro
 		onInit : function () {
 			this.getOwnerComponent().getRouter().getRoute("master").attachPatternMatched(this._onRouteMatched, this);
 
-			//Load OData Service
-			var oModel = new sap.ui.model.odata.ODataModel(serviceUrl, true);
-			sap.ui.getCore().setModel(oModel);
-
-		
-	
+			var oModel = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/ZOSO_PURCHASEORDER/A_PurchaseOrder");
+			this.getView().setModel(oModel, "user");
 			
-			//Get list control reference
-			var list = this.getView().byId("list");
-			
-			//Frame Url with EntitySet
-			var url = serviceUrl + "A_PurchaseOrder";
-					
-			//Call OdataService
-			OData.read(url, function(data) {
-	
-				//Read output
-				var result = data.results;
-				
-				//set JSONoutput to a JSONModel
-				var oModel = new sap.ui.model.json.JSONModel();
-				oModel.setData({
-					listItems : result
-				});
-				
-				//Set output to ListControl			
-				list.setModel(oModel);
-							
-			}, function(err) {
-				var errTxt = err.message + "\n" + err.request.requestUri;
-				sap.m.MessageBox.show(errTxt, sap.m.MessageBox.Icon.ERROR, "Service Error");
-			});	
 		},
 		_onRouteMatched: function(oEvent) {
 			/*
@@ -52,10 +23,12 @@ sap.ui.define( ["sap/ui/core/mvc/Controller", "sap/ui/Device"], function (Contro
 			}
 		},
 		onSelectionChange: function(oEvent) {
-			var sOrderId = oEvent.getSource().getSelectedItem().getBindingContext().getProperty("orderId");
+			//var sOrderId = oEvent.getSource().getSelectedItem().getBindingContext().getProperty("PurchaseOrder");
+			var productPath = oEvent.getSource().getBindingContext("PurchaseOrder");
+			//this.getView().byId("list").getValue();
 			this.getOwnerComponent().getRouter()
 				.navTo("orderDetails",
-					{orderId:sOrderId},
+					{PurchaseOrder:productPath},
 					!Device.system.phone);
 		}
 	});
