ed6d0c5c67d97846256ef12cdb89b35ee2bc67b0
rwiebe99 <richard.wiebe@student.fhws.de>
2022-05-24 17:28:30 +0200

    detail

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -8,15 +8,18 @@ sap.ui.define( ["sap/ui/core/mvc/Controller","sap/ui/core/routing/History", "sap
 			var oModel = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/ZOSO_PURCHASEORDER/A_PurchaseOrder");
 			this.getView().setModel(oModel, "user");
 
-			var oModelItem = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/ZOSO_PURCHASEORDER/A_PurchaseOrderItem");
-			this.getView().setModel(oModelItem, "item");
+			
 		},
 		
 		_onRouteMatched: function(oEvent) {
 			this._product = oEvent.getParameter("arguments").product;
 			this.getView().bindElement( "user>/d/results/" + this._product);
-			this.getView().bindElement( "item>/d/results/" + this._product);
+			var order = this.get
 
+			//https://S4H.OSOSOFT.DE:44300/sap/opu/odata/sap/ZOSO_PURCHASEORDER/A_PurchaseOrder('4500000001')/to_PurchaseOrderItem
+			//this.getView().bindElement( "item>/d/results/" + this._product);
+			var oModelItem = new sap.ui.model.json.JSONModel("/sap/opu/odata/sap/ZOSO_PURCHASEORDER/A_PurchaseOrder('"+ order +"')/to_PurchaseOrderItem");
+			this.getView().setModel(oModelItem, "item");
 		},
 		onSelectionChange: function(oEvent) {
 			var sProductId = oEvent.getSource().getBindingContext().getProperty("productId");
